8312743f16ca290330c50dd8d21591b6481d0707
Теодор Манахилов <tmanahilov@gmail.com>
2025-05-21 14:22:22 +0300

    Email notification feature

---
+++ crypto/crypto/src/main/java/com/exchange/crypto/service/EmailNotificationService.java
@@ -0,0 +1,42 @@
+package com.exchange.crypto.service;
+
+import jakarta.mail.*;
+import jakarta.mail.internet.*;
+import java.util.Properties;
+
+public class EmailNotificationService {
+
+    public static void sendEmail(String to, String subject, String body) {
+        final String from = "crypto.exchange.notif@gmail.com";
+        final String password = "lnmr gwko hlqa ofrn";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props,
+                new Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(from, password);
+                    }
+                });
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            message.setRecipients(
+                    Message.RecipientType.TO, InternetAddress.parse(to));
+            message.setSubject(subject);
+            message.setText(body);
+
+            Transport.send(message);
+
+            System.out.println("Email sent successfully");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
